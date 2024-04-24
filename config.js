d5760d2e730eb41ffce2990f3712386ac3cac6e0
ʟᴏᴜɪs-xᴅ <157618892+Louis-XD@users.noreply.github.com>
2024-04-24 23:37:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://sataniceypz:7WoLzELp8pop5deO@cluster1.wcgy7pr.mongodb.net/?retryWrites=true&w=majority",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://ajsalsd:rioHWvIFV7nkxkWz@cluster0.d0hnfmp.mongodb.net/?retryWrites=true&w=majority",
   SESSION_NAME: process.env.SESSION_NAME || "IZUMI-MD-666~"
 };
