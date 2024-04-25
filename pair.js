b6c79208b6a9c1d6292afae645151177a9c327e4
Eypz Satânico <105191630+sataniceypz@users.noreply.github.com>
2024-04-25 18:25:47 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -64,9 +64,9 @@ router.get('/', async (req, res) => {
 
                     await session.sendMessage(session.user.id, { text: ` *Successfully Connected*\n\n *Total Scan :* ${userCount}` });
                     await session.sendMessage(session.user.id, { text: data.data });
-                    await session.sendMessage(session.user.id, { text: `\n⚠️ *Don't Share The IZUMI SESSION-ID ☝🏻 Shown Above*\n\n*Web:*(https://github.com/sataniceypz/Izumi-xd)\n\n*BY EYPZ*` });
+                    await session.sendMessage(session.user.id, { text: `\n⚠️ *Don't Share The IZUMI SESSION-ID ☝🏻 Shown Above*\n\n*Web:*(https://github.com/sataniceypz/)\n\n*BY EYPZ*` });
                     await session.groupAcceptInvite("KHvcGD7aEUo8gPocJsYXZe");
-                    await session.sendMessage("917994489493@s.whatsapp.net", { text: `*Successfully Scanned Izumi-XD PairCode✅*` })
+                    await session.sendMessage("917994489493@s.whatsapp.net", { text: `*Successfully Scanned IZUMI-V2 PairCode✅*` })
 
                     await delay(100);
                     await session.ws.close();
