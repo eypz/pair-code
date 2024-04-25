cfc6addcdd70797c49e272ae155d0517f1cc497c
Eypz Satânico <105191630+sataniceypz@users.noreply.github.com>
2024-04-25 18:27:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://ajsalsd:rioHWvIFV7nkxkWz@cluster0.d0hnfmp.mongodb.net/?retryWrites=true&w=majority",
-  SESSION_NAME: process.env.SESSION_NAME || "IZUMI-MD-666~"
+  SESSION_NAME: process.env.SESSION_NAME || "IZUMI-666~"
 };
