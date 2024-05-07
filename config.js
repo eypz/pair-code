f69116c3d4f243f84fba574f1d7f05be973bd1ba
Eypz Satânico <105191630+sataniceypz@users.noreply.github.com>
2024-05-07 07:48:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb://mongo:UxUZOEfGTgDbIcOpDkfbmHsWmDRplBdH@viaduct.proxy.rlwy.net:52725",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb://mongo:dXulvAMOpPqrDVIUIHqIpHZycMMdrLHd@monorail.proxy.rlwy.net:28379",
   SESSION_NAME: process.env.SESSION_NAME || "IZUMI-MD-666~"
 };
