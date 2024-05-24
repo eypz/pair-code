72c14878288b0b01125275fae900edaa609e6f8b
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-05-24 10:14:14 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -6,7 +6,6 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const pino = require("pino");
-const { version } = await fetchLatestBaileysVersion();
 const {
     default: makeWASocket,
     useMultiFileAuthState,
